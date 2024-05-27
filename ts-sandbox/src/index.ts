f1bd35bd3863c193f43c3ee88cc466808e87a249
koheisugawra <51276845+suzy1031@users.noreply.github.com>
2024-05-27 21:20:57 +0000

    practice module pattern

--- ts-sandbox/src/index.ts
+++ ts-sandbox/src/index.ts
@@ -1,2 +1,6 @@
-const str: string = "hello world";
-console.log(str);
+import * as math from "./module-pattern/math";
+
+console.log(math.default(7, 8));
+console.log(math.multiply(8, 9));
+console.log(math.subtract(10, 3));
+console.log(math.square(3));
---
+++ ts-sandbox/src/module-pattern/math.ts
@@ -0,0 +1,15 @@
+export default function add(x: number, y: number): number {
+  return x + y;
+}
+
+export function multiply(x: number, y: number): number {
+  return x * y;
+}
+
+export function subtract(x: number, y: number): number {
+  return x - y;
+}
+
+export function square(x: number): number {
+  return x * x;
+}
