2cfa829ab7cad0d9d0911031cfa9e60760e6b9e5
koheisugawra <51276845+suzy1031@users.noreply.github.com>
2024-08-18 22:00:53 +0000

    refactor: Add Employee and ScrumTeam classes for managing company employees and scrum teams

---
+++ packages/core/src/company/employee.ts
@@ -0,0 +1,51 @@
+import { Id } from "../common/id";
+
+export class EmployeeId extends Id {
+  constructor(public readonly value: number | null) {
+    super(value);
+  }
+
+  static createAsNull() {
+    return new EmployeeId(null);
+  }
+
+  equals(id: EmployeeId) {
+    return this.value === id.value;
+  }
+}
+
+export class EmployeeName {
+  constructor(
+    public readonly firstName: string,
+    public readonly familyName: string
+  ) {}
+
+  static createFromString(name: string) {
+    if (!name.includes(" ")) {
+      throw new Error("社員名は姓名を半角スペースで区切ってください");
+    }
+
+    const [familyName, ...rest] = name.split(" ");
+
+    return new EmployeeName(rest.join(" "), familyName);
+  }
+}
+
+export class Employee {
+  constructor(
+    public readonly id: EmployeeId,
+    public readonly employeeName: EmployeeName
+  ) {}
+
+  updateName(name: EmployeeName) {
+    return new Employee(this.id, name);
+  }
+}
+
+export class Member {
+  constructor(public readonly employee: Employee) {}
+
+  static createFromEmployee(employee: Employee) {
+    return new Member(employee);
+  }
+}
